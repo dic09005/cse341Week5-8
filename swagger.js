a1f0b4c2e36b85adfb4a973650c500137b40d1ad
Robin Dickson <dic09005@byui.edu>
2023-05-16 16:29:21 -0600

    commit

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Contacts API',
+    description: 'NHL Players API',
   },
   host: 'dickson341test.onrender.com',
   schemes: ['https'],
